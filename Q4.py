b25c756f176a54c4b10093cfc45a66742f734127
rfire <rfire01@gmail.com>
2018-06-15 17:40:56 +0300

    Q4 updated

--- Q4.py
+++ Q4.py
@@ -6,18 +6,19 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 IMAGE_SHAPE = [-1, 28, 28, 1]
 POST_CONV_SIZE = [-1, 7 * 7 * 64]
-x_validation = None
-y_validation = None
-last_loss = 0
-before_last_loss = 0
 
 
-
-def create_conv_layer(x, row, col, filters):
+def create_conv_layer(x, row, col, filters, mode):
     conv = tf.layers.conv2d(inputs=x, filters=filters, kernel_size=[row, col],
-                            padding="same", activation=tf.nn.relu)
+                            padding="same",
+                            kernel_initializer=tf.contrib.layers.xavier_initializer())
 
-    pool = tf.layers.max_pooling2d(inputs=conv, pool_size=[2, 2], strides=2)
+    norm = tf.layers.batch_normalization(conv,
+                                         training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    norm = tf.nn.relu(norm)
+
+    pool = tf.layers.max_pooling2d(inputs=norm, pool_size=[2, 2], strides=2)
 
     return pool
 
@@ -34,23 +35,22 @@ def create_dense_layer(x, size, output=False):
     return dense
 
 
-def create_cnn_net(input_layer):
-    conv1 = create_conv_layer(input_layer, 5, 5, 32)
-    conv2 = create_conv_layer(conv1, 5, 5, 64)
+def create_cnn_net(input_layer, mode):
+    conv1 = create_conv_layer(input_layer, 5, 5, 32, mode)
+    conv2 = create_conv_layer(conv1, 5, 5, 64, mode)
     flattened = tf.reshape(conv2, POST_CONV_SIZE)
     dense1 = create_dense_layer(flattened, 1024)
-    dense2 = create_dense_layer(dense1, 1024)
+    dropout = tf.layers.dropout(inputs=dense1, rate=0.4,
+                                training=mode == tf.estimator.ModeKeys.TRAIN)
+    dense2 = create_dense_layer(dropout, 1024)
     out = create_dense_layer(dense2, 10, output=True)
 
     return out
 
 
 def create_training_net(features, labels, mode):
-
-    # print('entered create_training_net !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-
     input_layer = tf.reshape(features, IMAGE_SHAPE)
-    cnn_net = create_cnn_net(input_layer)
+    cnn_net = create_cnn_net(input_layer, mode)
 
     predictions = tf.argmax(input=cnn_net, axis=1)
 
@@ -74,9 +74,7 @@ def create_training_net(features, labels, mode):
                                           training_hooks=[cost_logging])
 
     metrics = {"accuracy": tf.metrics.accuracy(labels=labels,
-                                                       predictions=predictions),
-               "precision": tf.metrics.precision(labels, predictions)
-               }
+                                               predictions=predictions)}
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss,
                                       eval_metric_ops=metrics)
 
@@ -84,26 +82,27 @@ def create_training_net(features, labels, mode):
 def main(unused_argv):
     mnist = tf.contrib.learn.datasets.load_dataset("mnist")
 
-    global x_validation
-    global y_validation
-    global last_loss
-    global before_last_loss
+    last_loss = np.inf
+    before_last_loss = np.inf
 
     x_train = mnist.train.images  # Returns np.array
     y_train = np.asarray(mnist.train.labels, dtype=np.int32)
 
-    validation_size = int(x_train.shape[0] * 0.2)
-    validation_idx = []
-    while len(validation_idx) != validation_size:
-        idx = np.random.randint(0, x_train.shape[0])
-        if idx not in validation_idx:
-            validation_idx.append(idx)
+    x_validation = mnist.validation.images
+    y_validation = np.asarray(mnist.validation.labels, dtype=np.int32)
 
-    x_validation = x_train[validation_idx, :]
-    y_validation = y_train[validation_idx, ]
-
-    x_train = x_train[[x for x in range(x_train.shape[0]) if x not in validation_idx]]
-    y_train = y_train[[y for y in range(y_train.shape[0]) if y not in validation_idx]]
+    # validation_size = int(x_train.shape[0] * 0.2)
+    # validation_idx = []
+    # while len(validation_idx) != validation_size:
+    #     idx = np.random.randint(0, x_train.shape[0])
+    #     if idx not in validation_idx:
+    #         validation_idx.append(idx)
+    #
+    # x_validation = x_train[validation_idx, :]
+    # y_validation = y_train[validation_idx, ]
+    #
+    # x_train = x_train[[x for x in range(x_train.shape[0]) if x not in validation_idx]]
+    # y_train = y_train[[y for y in range(y_train.shape[0]) if y not in validation_idx]]
 
     x_test = mnist.test.images  # Returns np.array
     y_test = np.asarray(mnist.test.labels, dtype=np.int32)
@@ -111,31 +110,29 @@ def main(unused_argv):
     classifier = tf.estimator.Estimator(model_fn=create_training_net,)
                                       # config=tf.contrib.learn.RunConfig(save_checkpoints_steps=10))
 
-
-
     train_net = tf.estimator.inputs.numpy_input_fn(x=x_train,
                                                  y=y_train,
                                                  batch_size=100,
                                                  num_epochs=None,
                                                  shuffle=True)
 
+    validation_net = tf.estimator.inputs.numpy_input_fn(x=x_validation,
+                                                        y=y_validation,
+                                                        num_epochs=1,
+                                                        shuffle=False)
 
     for i in range(5000):
         classifier.train(input_fn=train_net, steps=1)
-        validation_net = tf.estimator.inputs.numpy_input_fn(x=x_validation,
-                                                y=y_validation,
-                                                num_epochs=1,
-                                                shuffle=False)
+        print("trained starting to evaluate")
         validation_res = classifier.evaluate(input_fn=validation_net)
-        # print('int iteration ', i, ' loss is = ', validation_res['loss'])
-        if before_last_loss == last_loss == validation_res['loss']:
+        print('int iteration ', i, ' loss is = ', validation_res['loss'])
+        if before_last_loss <= last_loss <= validation_res['loss']:
             print('no improvement for 3 batches in a row')
             print('stopping training')
             break;
 
         before_last_loss = last_loss
         last_loss = validation_res['loss']
-
 
     test_net = tf.estimator.inputs.numpy_input_fn(x=x_test,
                                                 y=y_test,
